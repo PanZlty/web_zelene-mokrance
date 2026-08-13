5953db950be40a24a89685c50e45c33a02526e81
Dvade <dvade@users.noreply.github.com>
2026-08-13 19:30:58 +0200

    Remove nested hero stats divider

--- mu-plugins/zelene-mokrance-homepage.php
+++ mu-plugins/zelene-mokrance-homepage.php
@@ -98,6 +98,7 @@ add_action( 'wp_enqueue_scripts', function () {
 .zm-home__hero{min-height:unset!important}
 .zm-home__hero-stats strong,.zm-home__hero-stats span{font-family:inherit!important;color:inherit!important}.zm-home__hero-stats strong{font-size:1em!important}.zm-home__hero-stats span{font-size:.55em!important}
 .zm-home__hero-stats .zm-home__hero-stat-label{display:block!important;padding:0!important;margin-top:7px!important}
+.zm-home__hero-stats>div>div{border:0!important}
 ' );
 }, 30 );
 
